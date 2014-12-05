1aa07a13ff2d634440416d1b9d2a5a36fced6f20
Jürgen <juergen.ullmann93@gmx.de>
2014-12-05 16:32:13 +0100

    Timer
    -- Still Untested Changes --
    On Stefans  proposal last changes are now synchronized.

--- src/com/example/Demo_Subj/RoomActivity.java
+++ src/com/example/Demo_Subj/RoomActivity.java
@@ -106,6 +106,19 @@ public class RoomActivity extends Activity {
                 else{
                     //Do nothing
                 }
+                synchronized (mPauseLock) {
+                    while (!running) {
+                        try {
+                            mPauseLock.wait();              //Wenn ich das richtig verstehe, wird hier versucht, mPauseLock
+                                                            //solange zu pausieren(xyz.wait() legt eine Thread schlafen bis
+                                                            //er durch xyz.notify() geweckt wird), wie running false ist.
+                                                            //Theoretisch sollte es ausreichen, den Thread einmal schlafen zu legen
+                                                        
+                        } catch (InterruptedException e) {
+
+                        }
+                    }
+                }
             }
         }, 0, tick);
 
@@ -145,20 +158,18 @@ public class RoomActivity extends Activity {
     @Override
     public void onPause() {
         super.onPause();
-        running = false;
-        /*synchronized (mPauseLock) {
-            mPaused = true;
-        }*/
+        synchronized (mPauseLock) {
+            running = false;
+        }
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        running = true;
-        /*synchronized (mPauseLock) {
-            mPaused = false;
+        synchronized (mPauseLock) {
+            running = true;
             mPauseLock.notifyAll();
-        }*/
+        }
     }
 
     @Override
